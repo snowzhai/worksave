0849fd46d5a0bb7dd1f277a54dc3657b410be7fa
zhai <1689814801@qq.com>
2017-08-16 22:48:43 +0800

    增加任务功能 提醒

--- functions/src/main/java/com/zjxd/functions/task/service/MyService.java
+++ functions/src/main/java/com/zjxd/functions/task/service/MyService.java
@@ -78,7 +78,7 @@ public class MyService extends Service {
         @Override
         public void onReceive(Context context, Intent intent) {
             if(intent.ACTION_TIME_TICK.equals(intent.getAction())){
-                ShowUtils.i("wordsave"," 时间变化了22223333");
+                ShowUtils.i("wordsave"," 时间变化了2222333344444");
                 mHelp = new mSqliteHelp(context);
                 data = mHelp.queryTask(StringUtils.formatDateNow("yyyyMMdd"),1,2);
                 ShowNotifition(context,data.length);
